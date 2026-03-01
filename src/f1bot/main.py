b5a08d366d4cb173a7a07d16435389f66096f0f9
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2026-02-28 16:46:01 -0800

    Add permissions statement to main.py

--- src/f1bot/main.py
+++ src/f1bot/main.py
@@ -46,6 +46,8 @@ async def main() -> None:
                 await bot.load_extension(f'src.f1bot.commands.{command.name[:-3]}')
         # endregion
 
+        bot.tree.default_permissions = None
+
     @bot.event
     async def on_ready():
         print("Ready")
